c35b967db9dc04464b2b9d1c3cb1fabf6c20ee74
Sarka Filipiova <sarkafilipiova@Sarka-Air.lan>
2021-05-26 16:25:08 +0200

    favi x

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     // Zkopírovat css/ to _site/css/
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("favicon");
    
 
     return {
